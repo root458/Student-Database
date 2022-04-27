b560dd849d0761d856c16b0ce3063590a11c0bb2
Hope Kagame <kagame458@protonmail.com>
2022-04-27 17:07:53 +0300

    Initialize view Instantiate SQLiteHelper

--- app/src/main/java/com/suis/studentdatabase/MainActivity.kt
+++ app/src/main/java/com/suis/studentdatabase/MainActivity.kt
@@ -2,10 +2,30 @@ package com.suis.studentdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edName: EditText
+    private lateinit var edMail: EditText
+    private lateinit var btnAdd: Button
+    private lateinit var btnView: Button
+
+    private lateinit var sqLiteHelper: SQLiteHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        sqLiteHelper = SQLiteHelper(context = this)
+    }
+
+    private fun initView() {
+        edName = findViewById(R.id.edName)
+        edMail = findViewById(R.id.edEmail)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnView = findViewById(R.id.btnView)
     }
 }
