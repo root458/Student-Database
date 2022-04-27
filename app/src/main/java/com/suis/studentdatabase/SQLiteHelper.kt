89e88e5b8f57f25007099ff19aa09f955f323a65
Hope Kagame <kagame458@protonmail.com>
2022-04-27 16:24:51 +0300

    Add insert and fetch methods to SQLiteHelper.kt

--- app/src/main/java/com/suis/studentdatabase/SQLiteHelper.kt
+++ app/src/main/java/com/suis/studentdatabase/SQLiteHelper.kt
@@ -1,6 +1,8 @@
 package com.suis.studentdatabase
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
@@ -28,6 +30,52 @@ class SQLiteHelper(context: Context) :
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
         db!!.execSQL("DROP TABLE IF EXISTS $TBL_STUDENT")
         onCreate(db)
+    }
+
+    fun insertStudent(std: StudentModel): Long {
+        val db = this.writableDatabase
+        val contentValues = ContentValues()
+
+        contentValues.put(ID, std.id)
+        contentValues.put(NAME, std.name)
+        contentValues.put(EMAIL, std.email)
+
+        val success = db.insert(TBL_STUDENT, null, contentValues)
+        db.close()
+        return success
+    }
+
+    fun getAllStudents(): ArrayList<StudentModel> {
+        val stdList: ArrayList<StudentModel> = ArrayList()
+        val selectQuery = "SELECT * FROM $TBL_STUDENT"
+        val db = this.readableDatabase
+
+        val cursor: Cursor?
+
+        try {
+            cursor = db.rawQuery(selectQuery, null)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            db.execSQL(selectQuery)
+            return ArrayList()
+        }
+
+        var id: Int
+        var name: String
+        var email: String
+
+        if (cursor.moveToFirst()) {
+            do {
+                id = cursor.getInt(cursor.getColumnIndexOrThrow("id"))
+                name = cursor.getString(cursor.getColumnIndexOrThrow("name"))
+                email = cursor.getString(cursor.getColumnIndexOrThrow("email"))
+                val std = StudentModel(id = id, name = name, email = email)
+                stdList.add(std)
+            } while (cursor.moveToNext())
+        }
+
+        cursor.close()
+        return stdList
     }
 
 }
