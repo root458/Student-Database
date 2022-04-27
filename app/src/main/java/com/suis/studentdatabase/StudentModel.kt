c001685dab0e6fb750acb06f53a0b2c598b11303
Hope Kagame <kagame458@protonmail.com>
2022-04-27 15:51:05 +0300

    Add StudentModel class

---
+++ app/src/main/java/com/suis/studentdatabase/StudentModel.kt
@@ -0,0 +1,16 @@
+package com.suis.studentdatabase
+
+import java.util.*
+
+data class StudentModel(
+    var id: Int = getAutoId(),
+    var name: String = "",
+    var email: String = ""
+) {
+    companion object {
+        fun getAutoId(): Int {
+            val random = Random()
+            return random.nextInt(100)
+        }
+    }
+}
