de705d431a0b9bf2ea7e6bac4f563f44bef519aa
Hope Kagame <kagame458@protonmail.com>
2022-04-27 17:45:22 +0300

    Add onClickEvents for btnAdd and btnView Students can be added to database

--- app/src/main/java/com/suis/studentdatabase/MainActivity.kt
+++ app/src/main/java/com/suis/studentdatabase/MainActivity.kt
@@ -2,8 +2,10 @@ package com.suis.studentdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +22,46 @@ class MainActivity : AppCompatActivity() {
 
         initView()
         sqLiteHelper = SQLiteHelper(context = this)
+
+        btnAdd.setOnClickListener { addStudent() }
+        btnView.setOnClickListener { getStudents() }
+    }
+
+    private fun getStudents() {
+        val stdList = sqLiteHelper.getAllStudents()
+        Log.i("Size", "${stdList.size}")
+    }
+
+    private fun addStudent() {
+        val name = edName.text.toString()
+        val email = edMail.text.toString()
+        if (name.isEmpty() || email.isEmpty()) {
+            Toast.makeText(
+                this, "Please fill out the fields",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            val std = StudentModel(name = name, email = email)
+            val status = sqLiteHelper.insertStudent(std)
+            if (status > -1) {
+                Toast.makeText(
+                    this, "Student successfully added",
+                    Toast.LENGTH_SHORT
+                ).show()
+                clearEditText()
+            } else {
+                Toast.makeText(
+                    this, "Please try again",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun clearEditText() {
+        edName.setText("")
+        edMail.setText("")
+        edName.requestFocus()
     }
 
     private fun initView() {
